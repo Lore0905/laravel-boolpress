bdb3e0c3edc7091679b0e4a001a034a862ab6881
Lore0905 <lore.castelli4@gmail.com>
2022-03-02 16:17:51 +0100

    API

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     public function index(){
-        dd('funziona tutto correttamente');
+        // prendo dal model Post tutta la tabella 
+        $posts = Post::all();
+        // la trasformo in un array di oggettti e lo mettiamo in un file json
+        return response()->json([
+            'success' => true,
+            'posts' => $posts
+        ]);
     }
 }
